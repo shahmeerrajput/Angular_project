cf60524e87cdfc8fb122da1bbce6f52b12d73b74
a.samad <48627078+shahmeerrajput@users.noreply.github.com>
2022-08-22 17:58:18 +0500

    update likes and change detection

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Params} from '@angular/router';
 import { map } from 'rxjs/operators';
 import { PostService,Authfetch, AuthLike } from '../post.service';
 
@@ -10,37 +10,28 @@ import { PostService,Authfetch, AuthLike } from '../post.service';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  like_counter:AuthLike;
-  like:number;
-  last_like:AuthLike;
-  counter:number = 0;
   user_id : {id:string};
   loadedPost: Authfetch[]=[];
-  loadedLike:AuthLike[] = [];
+  
   createPost = false; 
   constructor(private post:PostService,
               private route_user:ActivatedRoute) { }
 
   ngOnInit(){
-    this.user_id = {
-      id:this.route_user.snapshot.params['id']
-    };
-    this.post.fetchLike().pipe(map(resdata=>{
-      const postArray:AuthLike[] = [];
-      for(const key in resdata){
-        if(resdata.hasOwnProperty(key)){
-          postArray.push({ ...resdata[key]});
-        }
-      }
-      return postArray;  
-    })).subscribe(post=>{
-      console.log(post);
-      this.loadedLike = post;
-      this.last_like = this.loadedLike[this.loadedLike.length-1];
-      this.like = this.last_like.like;
-    });
-
-
+    this.fetchPost();
+  }
+  createpost(){
+    this.createPost = !this.createPost;
+  }
+  onSubmit(form:NgForm){
+    const value = form.value;
+    this.post.postRequest(value).subscribe(resData => {
+      console.log(resData);
+      this.fetchPost();
+      });
+  }
+  
+  fetchPost(){
     this.post.fetchdata().pipe(map(resdata=>{
       const postArray:Authfetch[] = [];
       for(const key in resdata){
@@ -52,27 +43,14 @@ export class HomePageComponent implements OnInit {
     })).subscribe(post=>{
       console.log(post)
       this.loadedPost = post;
-    });    
-
+    });  
   }
-
-
-  createpost(){
-    this.createPost = !this.createPost;
+  fetchid(){
+    this.user_id = {
+      id:this.route_user.snapshot.params['id']
+    };
+    this.route_user.params.subscribe((params:Params)=>{
+      this.user_id.id=params['id'];
+    });
   }
-
-  onSubmit(form:NgForm){
-    const value = form.value;
-    this.post.postRequest(value).subscribe(resData => {
-      console.log(resData)});
-  }
-
-  likedButton(){
-    this.counter = this.counter + 1;
-    this.like_counter = {like:this.counter,id:this.user_id.id}
-    this.post.postLike(this.like_counter).subscribe(resdata=>{
-      console.log(resdata);
-    })
-  }
-
 }
